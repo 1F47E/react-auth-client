ff65256c831cf91bee9a65486dbfe451d098264f
kas <32327548+kaspar1ndustries@users.noreply.github.com>
2022-11-25 16:21:31 +0800

    env from vercel

--- services/client.ts
+++ services/client.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+// get the url of the websocket server from vercel envs
+// NEXT_PUPLIC prefix is required for vercel to expose the env
+const SERVER_HOST = process.env.NEXT_PUBLIC_SERVER_HOST as string;
+
 export default axios.create({
     // TODO: move to env
-    baseURL: "http://localhost:8000",
+    baseURL: SERVER_HOST,
     timeout: 5000,
     withCredentials: false // have to stay false for CORS reasons
 });
